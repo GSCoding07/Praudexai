537ad9ac1d12b3cd74e431bafc0102e5af3db2ca
GSCoding07 <gitansh.sawdekar@gmail.com>
2024-02-24 10:33:20 -0800

    added css, solved the problem which was that I had to add static.

--- website/views.py
+++ website/views.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, render_template
 
-views = Blueprint('views',__name__)
+views = Blueprint("views",__name__)
 
 site_name = "Praudexai"
-@views.route('/')
+
+@staticmethod
+@views.route("/")
 def entry():
     return render_template("entry.html", site_name = site_name)
 
